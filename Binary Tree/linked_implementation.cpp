babf58e20e0b1d5af1e8f36c85033c8db37715d9
Shuva Dev <s.r.shuvadeb@gmail.com>
2024-01-28 17:28:14 +0600

    Linked list implementation of binary tree added

--- Binary Tree/linked_implementation.cpp
+++ Binary Tree/linked_implementation.cpp
@@ -1,9 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
+// implementation of binary tree in linked list using recursive procedure
 
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+};
+
+class BinaryTree {
+    public:
+        Node * createTree() {
+            int input, data;
+            cout << "Do you want to add a node? (0 for no and 1 for yes) : ";
+            cin >> input;
+
+            if(input == 0) {
+                return NULL;
+            } else {
+                Node *newNode = new Node();
+                cout << "Enter the data : "; cin >> data;
+                newNode -> data = data;
+                cout << "Left child of " << newNode -> data << "\n";
+                newNode -> left = createTree();
+                cout << "Right child of " << newNode -> data << "\n";
+                newNode -> right = createTree();
+                return newNode;
+            }
+        }
+};
 
 int main() {
-    
+    BinaryTree t;
+    Node *root = t.createTree();
+    cout << root -> data << endl;
+    cout << root -> left -> data << endl;
+    cout << root -> right -> data << endl;
     
     
     
